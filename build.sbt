cd9af3b493c81a3a454c247dd0c92d4bf99f3b31
Patrick Roemer <patrick@sangamon.de>
2021-09-09 00:39:04 +0200

    step 03: derivation

--- build.sbt
+++ build.sbt
@@ -2,6 +2,7 @@ ThisBuild / scalaVersion     := "3.0.1"
 ThisBuild / version          := "0.1.0-SNAPSHOT"
 ThisBuild / organization     := "de.sangamon"
 
+ThisBuild / scalacOptions ++= Seq("-unchecked", "-deprecation", "-Xfatal-warnings", "-source:future")
 
 lazy val root = (project in file("."))
   .settings(
--- src/main/scala/de/sangamon/nanocsv/step03/CSVParserMain.scala
+++ src/main/scala/de/sangamon/nanocsv/step03/CSVParserMain.scala
@@ -1,16 +1,18 @@
-package de.sangamon.nanocsv.step01
+package de.sangamon.nanocsv.step03
 
+import cats.syntax.all.*
 import de.sangamon.nanocsv.shared.*
 
 import java.nio.file.*
+import java.time.LocalDate
 
-object CSVParseMain:
+object CSVParserMain:
 
   import CSVParser.*
   import RowParser.*
+  import RowParserDerivable.*
 
-  val userParser: RowParser[User] =
-    const(User.apply.curried) <*> int <*> string <*> date
+  val userParser: RowParser[User] = User.apply.curried.deriveRowParser
 
   @main def main(csvFile: String): Unit =
     for(u <- CSVParser.parse(Paths.get(csvFile))(userParser))
